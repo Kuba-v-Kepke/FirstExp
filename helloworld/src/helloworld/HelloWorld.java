c91d87e7c2cd7dea38b683824a02bbeb3656df48
Kuba-V-Kepke <Alived2112@gmail.com>
2023-05-11 14:50:29 +0300

    Initial commit

---
+++ helloworld/src/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package helloworld;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		// вывод строки
+		System.out.println("Hello World!");
+	}
+	
+}
---
+++ helloworld/src/helloworld/TgrmTest_2.java
@@ -0,0 +1,35 @@
+package helloworld;
+
+public class TgrmTest_2 {
+	public static void main(String[] args){
+/*		String name = "Kostya";
+		System.out.println(name);*/ //Используя String
+		
+		
+/*		char ch = 'K';
+		System.out.println(ch);
+		
+		ch = 'o';
+		System.out.println(ch);
+		
+		ch = 's';
+		System.out.println(ch);
+		
+		ch = 't';
+		System.out.println(ch);
+		
+		ch = 'y';
+		System.out.println(ch);
+		
+		ch = 'a';
+		System.out.println(ch);*/ // Используя одну переменную char
+		
+		char ch1 = 'K';
+		char ch2 = 'o';
+		char ch3 = 's';
+		char ch4 = 't';
+		char ch5 = 'y';
+		char ch6 = 'a';
+		System.out.println("" + ch1 + ch2 + ch3 + ch4 + ch5 + ch6); // C использованием множества переменных char
+	}
+}
